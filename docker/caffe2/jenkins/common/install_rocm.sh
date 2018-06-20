9d2307b18003472a8c4081d8b85a8b3ad43c6cb9
Edward Z. Yang <ezyang@fb.com>
2018-06-20 16:45:50 -0400

    [TESTING] Support ROCm 1.8.2 prerelease docker image
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- docker/caffe2/jenkins/common/install_rocm.sh
+++ docker/caffe2/jenkins/common/install_rocm.sh
@@ -6,6 +6,48 @@ set -ex
 # it ignores all values of ROCM_VERSION which are not nightly.  Ugh!
 [ -n "$ROCM_VERSION" ]
 
+if [ "$ROCM_VERSION" = "1.8.2" ]; then
+  export DEBIAN_FRONTEND=noninteractive
+
+  apt-get update
+  apt-get install -y wget
+
+  DEB_ROCM_REPO=http://repo.radeon.com/rocm/apt/debian
+  wget -qO - $DEB_ROCM_REPO/rocm.gpg.key | apt-key add -
+
+  mkdir -p /opt/apt
+  pushd /opt/apt
+  curl http://repo.radeon.com/rocm/misc/beta_1.8.2/apt_1.8.2.tar.bz2 -o apt_1.8.2.tar.bz2
+  tar xf apt_1.8.2.tar.bz2
+  rm apt_1.8.2.tar.bz2
+  echo "deb file:/opt/apt/apt_1.8.2/debian xenial main" > /etc/apt/sources.list.d/rocm.list
+  popd
+
+  apt-get install -y \
+    rocm-dev \
+    rocm-libs \
+    rocm-utils \
+    rocfft \
+    miopen-hip \
+    miopengemm \
+    rocblas \
+    hipblas \
+    rocrand \
+    rocm-profiler \
+    cxlactivitylogger
+
+  pushd $HOME
+  curl https://s3.amazonaws.com/ossci-linux/hcrng-master-a8c6a0b-Linux.deb -o hcrng.deb
+  dpkg -i hcrng.deb
+  rm hcrng.deb
+  curl https://s3.amazonaws.com/ossci-linux/hcsparse-master-907a505-Linux.deb -o hcsparse.deb
+  dpkg -i hcsparse.deb
+  rm hcsparse.deb
+  popd
+
+  exit 0
+fi
+
 install_hip_nightly() {
     git clone https://github.com/ROCm-Developer-Tools/HIP.git
     pushd HIP
