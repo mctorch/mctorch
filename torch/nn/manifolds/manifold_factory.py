31657fd2847c34683ca59a1929af0e7ead48e5d7
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2018-08-28 21:17:13 +0530

    updated documentation strings and fixed stiefel factory bug

--- torch/nn/manifolds/manifold_factory.py
+++ torch/nn/manifolds/manifold_factory.py
@@ -3,6 +3,18 @@ from ..parameter import Parameter
 
 
 class ManifoldShapeFactory(object):
+    """
+    Base class for manifold shape factory. This is used by torch
+    modules to determine shape of Manifolds give shape of weight matrix
+
+    For each Manifold type it takes shape and whether to transpose the
+    tensor when shape is vague (in instances when both transpose and normal
+    are valid).
+
+    To register a new factory implement a new subclass and create its object
+    with manifold as parameter
+
+    """
     factories = {}
 
     @staticmethod
@@ -25,6 +37,18 @@ class ManifoldShapeFactory(object):
 
 
 class StiefelLikeFactory(ManifoldShapeFactory):
+    """
+    Stiefel like factory implements shape factory where tensor constrains are
+    similar to that of Stiefel.
+
+    Constraints:
+        if 3D tensor (k,h,w):
+            k > 1 and h > w > 1
+        if 2D tensor (h,w):
+            h > w > 1
+    in case of h == w both normal and tranpose are valid and user has
+    flexibility to choose if he wants (h x w) or (w x h) as manifold
+    """
     def create(self, shape, transpose=False):
         if len(shape) == 3:
             k, h, w = shape
@@ -39,14 +63,14 @@ class StiefelLikeFactory(ManifoldShapeFactory):
         to_return = None
         if h > w:
             to_transpose = False
-            to_return = Parameter(manifold=Stiefel(h, w, k=k))
+            to_return = Parameter(manifold=self.manifold(h, w, k=k))
         elif h < w:
             to_transpose = True
-            to_return = Parameter(manifold=Stiefel(w, h, k=k))
+            to_return = Parameter(manifold=self.manifold(w, h, k=k))
         elif h == w:
             # use this argument only in case when shape is vague
             to_transpose = transpose
-            to_return = Parameter(manifold=Stiefel(w, h, k=k))
+            to_return = Parameter(manifold=self.manifold(w, h, k=k))
 
         return to_transpose, to_return
 
--- torch/nn/modules/conv.py
+++ torch/nn/modules/conv.py
@@ -183,6 +183,11 @@ class Conv1d(_ConvNd):
         groups (int, optional): Number of blocked connections from input
             channels to output channels. Default: 1
         bias (bool, optional): If ``True``, adds a learnable bias to the output. Default: ``True``
+        weight_manifold: If set convolution weight matrix is constrained on manifold space. 
+            weight_manifold shape is `in_channels * (out_channels * kernel_size)` 
+            or transpose of it.
+        trnaspose_flas: This is used only when weight_manifold shape is vague
+            Default: ``False``
 
     Shape:
         - Input: :math:`(N, C_{in}, L_{in})`
@@ -312,6 +317,11 @@ class Conv2d(_ConvNd):
         dilation (int or tuple, optional): Spacing between kernel elements. Default: 1
         groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1
         bias (bool, optional): If ``True``, adds a learnable bias to the output. Default: ``True``
+        weight_manifold: If set convolution weight matrix is constrained on manifold space. 
+            weight_manifold shape is `in_channels * (out_channels * ks[0] *ks[1])` 
+            or transpose of it.
+        trnaspose_flas: This is used only when weight_manifold shape is vague
+            Default: ``False``
 
     Shape:
         - Input: :math:`(N, C_{in}, H_{in}, W_{in})`
@@ -445,6 +455,11 @@ class Conv3d(_ConvNd):
         dilation (int or tuple, optional): Spacing between kernel elements. Default: 1
         groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1
         bias (bool, optional): If ``True``, adds a learnable bias to the output. Default: ``True``
+        weight_manifold: If set convolution weight matrix is constrained on manifold space. 
+            weight_manifold shape is `in_channels * (out_channels * ks[0] * ks[1] * ks[2])` 
+            or transpose of it.
+        trnaspose_flas: This is used only when weight_manifold shape is vague
+            Default: ``False``
 
     Shape:
         - Input: :math:`(N, C_{in}, D_{in}, H_{in}, W_{in})`
@@ -626,6 +641,11 @@ class ConvTranspose1d(_ConvTransposeNd):
         groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1
         bias (bool, optional): If ``True``, adds a learnable bias to the output. Default: ``True``
         dilation (int or tuple, optional): Spacing between kernel elements. Default: 1
+        weight_manifold: If set convolution weight matrix is constrained on manifold space. 
+            weight_manifold shape is `out_channels * (in_channels * kernel_size)` 
+            or transpose of it.
+        trnaspose_flas: This is used only when weight_manifold shape is vague
+            Default: ``False``
 
     Shape:
         - Input: :math:`(N, C_{in}, L_{in})`
@@ -744,6 +764,11 @@ class ConvTranspose2d(_ConvTransposeNd):
         groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1
         bias (bool, optional): If ``True``, adds a learnable bias to the output. Default: ``True``
         dilation (int or tuple, optional): Spacing between kernel elements. Default: 1
+        weight_manifold: If set convolution weight matrix is constrained on manifold space. 
+            weight_manifold shape is `out_channels * (in_channels * ks[0] * ks[1])` 
+            or transpose of it.
+        trnaspose_flas: This is used only when weight_manifold shape is vague
+            Default: ``False``
 
     Shape:
         - Input: :math:`(N, C_{in}, H_{in}, W_{in})`
@@ -893,6 +918,11 @@ class ConvTranspose3d(_ConvTransposeNd):
         groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1
         bias (bool, optional): If ``True``, adds a learnable bias to the output. Default: ``True``
         dilation (int or tuple, optional): Spacing between kernel elements. Default: 1
+        weight_manifold: If set convolution weight matrix is constrained on manifold space. 
+            weight_manifold shape is `out_channels * (in_channels * ks[0] * ks[1] * ks[2])` 
+            or transpose of it.
+        trnaspose_flas: This is used only when weight_manifold shape is vague
+            Default: ``False``
 
     Shape:
         - Input: :math:`(N, C_{in}, D_{in}, H_{in}, W_{in})`
