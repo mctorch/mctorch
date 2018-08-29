pytorch/pytorch

mutable lists, take 2

torch/csrc/jit/interned_strings.h
@@ -55,6 +55,7 @@ _(prim, AnyDefined) \
 _(prim, FusedConcat) \
 _(prim, ListSelect) \
 _(prim, ListSlice) \
+_(prim, append) \
 _(aten, __not__) \
 FORALL_ATEN_BASE_SYMBOLS(_) \
 _(onnx, Add) \

mctorch/mctorch

torch/csrc/jit/interned_strings.h
@@ -56,6 +56,8 @@ namespace torch { namespace jit {
   _(prim, FusedConcat)             \
   _(prim, FusedChunk)              \
   _(prim, NoneGenerator)           \
+  _(prim, MemoryFence)             \
+  _(aten, append)                  \
   _(aten, __not__)                 \
   FORALL_ATEN_BASE_SYMBOLS(_)      \
   _(onnx, Add)                     \