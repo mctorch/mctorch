c48459dc38d16c4f82914e4ab68fae21bfe4de58
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2018-08-30 17:32:01 +0530

    updated adagrad to support manifold updates

--- torch/optim/adagrad.py
+++ torch/optim/adagrad.py
@@ -70,31 +70,38 @@ class Adagrad(Optimizer):
 
                 state['step'] += 1
 
-                if group['weight_decay'] != 0:
-                    if p.grad.data.is_sparse:
-                        raise RuntimeError("weight_decay option is not compatible with sparse gradients")
-                    grad = grad.add(group['weight_decay'], p.data)
-
                 clr = group['lr'] / (1 + (state['step'] - 1) * group['lr_decay'])
 
-                if grad.is_sparse:
-                    grad = grad.coalesce()  # the update is non-linear so indices must be unique
-                    grad_indices = grad._indices()
-                    grad_values = grad._values()
-                    size = grad.size()
+                if p.manifold is None:
+                    if group['weight_decay'] != 0:
+                        if p.grad.data.is_sparse:
+                            raise RuntimeError("weight_decay option is not compatible with sparse gradients")
+                        grad = grad.add(group['weight_decay'], p.data)
 
-                    def make_sparse(values):
-                        constructor = grad.new
-                        if grad_indices.dim() == 0 or values.dim() == 0:
-                            return constructor().resize_as_(grad)
-                        return constructor(grad_indices, values, size)
-                    state['sum'].add_(make_sparse(grad_values.pow(2)))
-                    std = state['sum'].sparse_mask(grad)
-                    std_values = std._values().sqrt_().add_(group['eps'])
-                    p.data.add_(-clr, make_sparse(grad_values / std_values))
+                    if grad.is_sparse:
+                        grad = grad.coalesce()  # the update is non-linear so indices must be unique
+                        grad_indices = grad._indices()
+                        grad_values = grad._values()
+                        size = grad.size()
+
+                        def make_sparse(values):
+                            constructor = grad.new
+                            if grad_indices.dim() == 0 or values.dim() == 0:
+                                return constructor().resize_as_(grad)
+                            return constructor(grad_indices, values, size)
+                        state['sum'].add_(make_sparse(grad_values.pow(2)))
+                        std = state['sum']._sparse_mask(grad)
+                        std_values = std._values().sqrt_().add_(1e-10)
+                        p.data.add_(-clr, make_sparse(grad_values / std_values))
+                    else:
+                        state['sum'].addcmul_(1, grad, grad)
+                        std = state['sum'].sqrt().add_(1e-10)
+                        p.data.addcdiv_(-clr, grad, std)
                 else:
-                    state['sum'].addcmul_(1, grad, grad)
-                    std = state['sum'].sqrt().add_(group['eps'])
-                    p.data.addcdiv_(-clr, grad, std)
-
+                    rgrad = p.rgrad.data
+                    state['sum'].add_(rgrad.pow(2))
+                    std = state['sum'].sqrt().add_(1e-10)
+                    modified_rgrad = p.manifold.proj(p.data, rgrad/std)
+                    p.data.add_(p.manifold.retr(p.data, -clr * modified_rgrad)
+                                - p.data)
         return loss
