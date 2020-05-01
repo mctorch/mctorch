9caf932b8e73143dddcf72674b1456361b474297
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2020-05-01 20:14:58 +0000

    naming it as torch package so other dependencies are resolved

--- setup.py
+++ setup.py
@@ -266,7 +266,7 @@ cmake_python_include_dir = distutils.sysconfig.get_python_inc()
 ################################################################################
 # Version, create_version_file, and package_name
 ################################################################################
-package_name = os.getenv('TORCH_PACKAGE_NAME', 'mctorch')
+package_name = os.getenv('TORCH_PACKAGE_NAME', 'torch')
 version = open('version.txt', 'r').read().strip()
 sha = 'Unknown'
 
