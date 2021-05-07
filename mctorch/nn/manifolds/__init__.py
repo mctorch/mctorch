423a875fef91c28be53ace4886650104eb54aa97
Satyadev Ntv <tvsatyadev@gmail.com>
2021-05-07 22:52:44 +0530

    Add support for DoublyStochastic manifold

--- mctorch/nn/manifolds/__init__.py
+++ mctorch/nn/manifolds/__init__.py
@@ -3,3 +3,4 @@ from .positive_definite import PositiveDefinite
 from .manifold_factory import create_manifold_parameter, manifold_random_
 from .euclidean import Euclidean
 from .hyperbolic import Hyperbolic
+from .doublystochastic import DoublyStochastic
